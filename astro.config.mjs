1dbd1f2f47436eecf1fd0ba865ca8fa15c235bdf
Pau Solé Ferrer <pausf9@gmail.com>
2026-04-05 23:58:43 +0200

    feat: add sitemap integration and robots.txt configuration for SEO optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: 'https://adsroicalc.com',
+  integrations: [react(), tailwind(), sitemap()],
   i18n: {
     defaultLocale: "es",
     locales: ["es", "ca"],
     routing: {
-        prefixDefaultLocale: false
+      prefixDefaultLocale: false
     }
   }
 });
